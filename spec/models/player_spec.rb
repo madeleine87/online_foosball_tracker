b5d4c22dd5f488a5277ec79d9dbf1b8ee91041a1
magdalena <magdalenawlodek87@gmail.com>
2015-06-03 19:26:26 +0200

    add two players tests

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -5,3 +5,19 @@ RSpec.describe Player, type: :model do
 		expect(player.valid?).to be false
 	end
 end
+
+RSpec.describe Player, type: :model do
+	it "can add two players with different email adress" do
+		playerA = FactoryGirl.create(:player_A)
+		playerB = FactoryGirl.create(:player_B)
+		expect(playerA.valid?).to be true
+		expect(playerB.valid?).to be true
+	end
+end
+
+RSpec.describe Player, type: :model do
+  it "can't create player with password length less than 8" do
+  	player = FactoryGirl.build(:player_A, password: "dom")
+  	expect(player.valid?).to be false
+	end
+end
